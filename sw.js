8f7de8c2fa970a335eb7bc4fdc2479b1037e1fa5
YannoteX <hugojeudy1491@gmail.com>
2023-12-18 22:35:54 +0100

    test PWA

--- sw.js
+++ sw.js
@@ -3,7 +3,6 @@ const CACHE_NAME = 'cool-cache';
 // Add whichever assets you want to precache here:
 const PRECACHE_ASSETS = [
     '/assets/',
-    '/css/'
 ]
 
 // Listener for the install event - precaches our assets list on service worker install.
