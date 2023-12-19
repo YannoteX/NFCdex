0d0b10c3b369a07f346783b297422cc652f28ef3
YannoteX <hugojeudy1491@gmail.com>
2023-12-19 22:02:26 +0100

    fix try for cache install event error

--- sw.js
+++ sw.js
@@ -11,6 +11,12 @@ const PRECACHE_ASSETS = [
 self.addEventListener('install', event => {
     event.waitUntil((async () => {
         const cache = await caches.open(CACHE_NAME);
-        cache.addAll(PRECACHE_ASSETS);
+        for (let i in PRECACHE_ASSETS){
+            try {
+                ok = await cache.add(i);
+            } catch (err) {
+                console.warn('sw: cache.add',i);
+            }
+        }
     })());
 });
