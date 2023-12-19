d01cc49deb7704a0e237e039b963731024fd2db8
YannoteX <hugojeudy1491@gmail.com>
2023-12-19 22:14:36 +0100

    fix try for cache install event error

--- sw.js
+++ sw.js
@@ -3,8 +3,6 @@ const CACHE_NAME = 'cool-cache';
 // Add whichever assets you want to pre-cache here:
 const PRECACHE_ASSETS = [
     '/assets/',
-    '/css/',
-    '/js/'
 ]
 
 // Listener for the install event - pre-caches our assets list on service worker install.
