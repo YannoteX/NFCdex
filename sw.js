db61df537989d6f82df5c609f4a96b98c37085e3
YannoteX <hugojeudy1491@gmail.com>
2023-12-20 00:47:16 +0100

    installation button

--- sw.js
+++ sw.js
@@ -45,23 +45,3 @@ self.addEventListener('fetch', event => {
         })
     );
 });
-
-// custom installation button
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    deferredPrompt = e;
-});
-
-const installApp = document.getElementById('installApp');
-
-installApp.addEventListener('click', async () => {
-    if (deferredPrompt !== null) {
-        deferredPrompt.prompt();
-        const { outcome } = await deferredPrompt.userChoice;
-        if (outcome === 'accepted') {
-            deferredPrompt = null;
-        }
-    }
-});
