2c82855f88dc1e78918763534d93698d54eba535
YannoteX <hugojeudy1491@gmail.com>
2023-12-19 22:16:08 +0100

    fix try for cache install event error

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'cool-cache';
 
 // Add whichever assets you want to pre-cache here:
 const PRECACHE_ASSETS = [
-    '/assets/',
+    '/assets/fonts/NotoSans_Condensed-Bold.ttf',
 ]
 
 // Listener for the install event - pre-caches our assets list on service worker install.
