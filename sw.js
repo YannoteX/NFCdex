eab480f8098cad143f0489dd37ef8b2dffd87dc5
YannoteX <hugojeudy1491@gmail.com>
2023-12-19 22:52:55 +0100

    claim debugging

--- sw.js
+++ sw.js
@@ -34,6 +34,7 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
