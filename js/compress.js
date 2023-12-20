e9d3099633b412f48d2b5742d5b6318cf7e3476f
YannoteX <hugojeudy1491@gmail.com>
2023-12-21 00:41:02 +0100

    test blob

--- js/compress.js
+++ js/compress.js
@@ -3,16 +3,10 @@ const input = document.getElementById("image");
 input.onclick = function() { this.value = null; };
 
 input.onchange = function(event) {
-
-    console.log(this.value)
     
     const file = event.target.files[0];
-
-    console.log(file)
   
     const blobURL = window.URL.createObjectURL(file);
-
-    console.log(blobURL);
 
     const img = new Image();
     img.src = file;
@@ -22,6 +16,8 @@ input.onchange = function(event) {
 
     img.onload = function(event){
         window.URL.revokeObjectURL(blobURL);
+        console.log(img.width);
+        console.log(img.height)
     }
 
     const canvas = document.createElement('canvas');
