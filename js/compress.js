ee1cc8a087df139f775b5138db8e093756b92546
YannoteX <hugojeudy1491@gmail.com>
2023-12-21 21:15:12 +0100

    testing resizing function

--- js/compress.js
+++ js/compress.js
@@ -12,22 +12,50 @@ input.onchange = function(event) {
     const blobURL = window.URL.createObjectURL(file);
 
     loadImage(blobURL).then(img => {
-        console.log(img.width);
-        console.log(img.height);
+
+        const canvas = document.createElement('canvas');
+        canvas.style.display = "none";
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        const newBlobURL = getShrinkImageBlob(canvas, img)
+
+        console.log(newBlobURL.length);
     });
-
-    const canvas = document.createElement('canvas');
-
-    canvas.style.display = "none";
 };
 
-function shrinkSize(canReduce, canvas, stop){
 
+function getShrinkImageBlob(canvas, image){
+
+    const context = canvas.getContext("2d");
+    let newBlobURL;
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+    canvas.toBlob(function (blob) {
+        newBlobURL = blob;
+    }, "image/webp", 0.4);
+
+    if (newBlobURL.length > 7100){
+
+        canvas.width /= 2;
+        canvas.height /= 2;
+
+        return getShrinkImage(canvas, image);
+    }
+    else if (newBlobURL.length < 6500){
+
+        canvas.width *= 1.5;
+        canvas.height *= 1.5;
+
+        return getShrinkImage(canvas, image);
+    }
+    else {
+        return newBlobURL
+    }
 }
 
-function reduceQuality(canvas){
-    
-}
 
 const loadImage = src =>
   new Promise((resolve, reject) => {
