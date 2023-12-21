5e2e7029a232c959a877f551cfddfeb6f6cbbc23
YannoteX <hugojeudy1491@gmail.com>
2023-12-21 21:59:15 +0100

    testing resizing function

--- js/compress.js
+++ js/compress.js
@@ -3,9 +3,6 @@ const input = document.getElementById("image");
 input.onclick = function() { this.value = null; };
 
 input.onchange = function(event) {
-    
-    let imgWidth = 0;
-    let imgHeight = 0;
 
     const file = event.target.files[0];
   
@@ -36,35 +33,42 @@ function getShrinkImageBlob(canvas, image){
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(image, 0, 0, canvas.width, canvas.height);
 
-    canvas.toBlob(function (blob) {
-        newBlobURL = blob;
-    }, "image/webp", 0.4);
+    let canvasBlob = getCanvasBlob(canvas);
 
-    if (newBlobURL.size > 7100){
+    getBlob.then(blob => {
+        if (newBlobURL.size > 7100){
 
-        canvas.width /= 2;
-        canvas.height /= 2;
+          canvas.width /= 2;
+          canvas.height /= 2;
 
-        return getShrinkImage(canvas, image);
-    }
-    else if (newBlobURL.size < 6500){
+          return getShrinkImage(canvas, image);
+        }
+        else if (newBlobURL.size < 6500){
 
-        canvas.width *= 1.5;
-        canvas.height *= 1.5;
+            canvas.width *= 1.5;
+            canvas.height *= 1.5;
 
-        return getShrinkImage(canvas, image);
-    }
-    else {
-        return newBlobURL
-    }
+            return getShrinkImage(canvas, image);
+        }
+        else {
+            return newBlobURL
+        }
+    });
 }
 
 
 const loadImage = src =>
-  new Promise((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = reject;
-    img.src = src;
-  })  
-;
+    new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = reject;
+        img.src = src;
+  });
+
+function getCanvasBlob(canvas) {
+  return new Promise(function(resolve, reject) {
+    canvas.toBlob(function(blob) {
+      resolve(blob)
+    }, "image/webp", 0.4);
+  })
+}
