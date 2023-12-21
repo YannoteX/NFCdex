f207f4781834ded456b4a1423f6dcce4c828175c
YannoteX <hugojeudy1491@gmail.com>
2023-12-21 22:29:05 +0100

    testing resizing function

--- js/compress.js
+++ js/compress.js
@@ -28,37 +28,32 @@ input.onchange = function(event) {
 function getShrinkImageBlob(canvas, image){
 
     const context = canvas.getContext("2d");
-    let newBlobURL;
 
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(image, 0, 0, canvas.width, canvas.height);
 
-    console.log(canvas.width);
-    console.log(canvas.height);
 
     getCanvasBlob(canvas).then(blob => {
 
-        console.log(blob.size);
 
         if (blob.size > 7100){
 
             canvas.width /= 2;
             canvas.height /= 2;
 
-            newBlobURL = getShrinkImageBlob(canvas, image);
+            return getShrinkImageBlob(canvas, image);
         }
         else if (blob.size < 6500){
 
             canvas.width *= 1.5;
             canvas.height *= 1.5;
 
-            newBlobURL = getShrinkImageBlob(canvas, image);
+            return getShrinkImageBlob(canvas, image);
         }
         else {
-            newBlobURL = blob;
+            return blob;
         }
 
-        return newBlobURL;
     });
 }
 
