ae27c9ecd1e4d7506ab54705c3d81ff8b45e506d
BadMisterH <badro-5@hotmail.com>
2024-01-22 00:18:45 +0100

    ee

--- js/NFC.js
+++ js/NFC.js
@@ -134,9 +134,11 @@ function updateView(jsonObject) {
   let Habitat = jsonObject.Habitat;
   let Description = jsonObject.Description;
 
+  document.querySelector(".Nom").innerHTML += Nom;
+  document.querySelector(".Description").innerHTML += Description;
   document.querySelector(
     ".imgSection"
-  ).innerHTML = `<img src="${ImageResult}"></img>`;
+  ).innerHTML += `<img src="${ImageResult}"></img>`;
   //   function getValueString(value) {
   //     console.log(value);
   //     if (typeof value === "object" && value !== null) {
