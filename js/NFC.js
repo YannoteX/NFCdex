e1bf87f08c6d8dadd8d60d11c01b9eccd596bb95
BadMisterH <badro-5@hotmail.com>
2024-01-21 22:34:41 +0100

    none

--- js/NFC.js
+++ js/NFC.js
@@ -152,7 +152,7 @@ function updateView(jsonObject) {
         })
         .join("");
     } else {
-      return String(value);
+      return console.log(value[4]);
     }
 
     function isImageUrl(value) {
