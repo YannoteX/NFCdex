13451bec3d0232825dbf772872be22cc373bcb79
BadMisterH <badro-5@hotmail.com>
2024-01-22 01:25:55 +0100

    ee

--- js/formValidate.js
+++ js/formValidate.js
@@ -162,7 +162,7 @@ export function resetForm(formId) {
         element.value = "";
       });
 
-    document.getElementById("imagePreview").src = ""
+    document.querySelector(".imgResult").src = ""
 
   } else {
     console.log("Le formulaire avec l'ID '" + formId + "' n'a pas été trouvé.");
