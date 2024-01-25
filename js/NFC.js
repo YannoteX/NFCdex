b5e6adce2cacdad4a63cc5af6fc72d99988b425c
BadMisterH <badro-5@hotmail.com>
2024-01-25 19:12:00 +0100

    test

--- js/NFC.js
+++ js/NFC.js
@@ -241,7 +241,7 @@ function updateView(jsonObject) {
 
 function NFCMessage(message, color = "#CF4307") {
     const messageContainer = document.getElementById("nfc-mode-message");
-    messageContainer.textContent = message;
+    messageContainer.innerHTML = `<p>message</p>`;
     messageContainer.style.color = color;
     messageContainer.classList.add("styleMessageScanner")
     messageContainer.setAttribute("data-content", message)
