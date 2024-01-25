948233fdfcde08fb87243b39810cd8a067b3c2a1
Hugo <53018953+YannoteX@users.noreply.github.com>
2024-01-25 11:26:25 +0100

    update manifest

--- sw.js
+++ sw.js
@@ -12,11 +12,17 @@ const PRECACHE_ASSETS = [
     'css/variables.css',
     'assets/fonts/pokemon-ds/pokemon-ds-font.ttf',
     'assets/icons/PhoneScreen2.webp',
+    'assets/icons/PhoneScreen2.png',
     'assets/icons/Pwa.svg',
     'assets/icons/desktopMod.webp',
     'assets/logo/anim1.svg',
     'assets/logo/Logo2.webp',
     'assets/logo/NFCdex_logo.webp',
+    'assets/PWA icons/icon-192x192.png',
+    'assets/PWA icons/icon-256x256.png',
+    'assets/PWA icons/icon-384x384.png',
+    'assets/PWA icons/icon-512x512.png',
+    'assets/PWA icons/maskable_icon_x512.png',
     'https://unpkg.com/aos@2.3.1/dist/aos.css'
 ]
 
