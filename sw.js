7eafcf72a7cb9c5261e872a60290e8509389ede3
Hugo <53018953+YannoteX@users.noreply.github.com>
2024-01-26 16:46:42 +0100

    fixing cache missingno

--- sw.js
+++ sw.js
@@ -2,23 +2,21 @@ const CACHE_NAME = 'NFCdex-offline';
 
 // Add whichever assets you want to pre-cache here:
 const PRECACHE_ASSETS = [
-    "/",
-    'index.html',
-    'js/NFC.js',
-    'js/formValidate.js',
-    'css/font.css',
-    'css/media-queries.css',
-    'css/style.css',
-    'css/variables.css',
-    'assets/fonts/pokemon-ds/pokemon-ds-font.ttf',
-    'assets/icons/PhoneScreen2.webp',
-    'assets/icons/PhoneScreen2.png',
-    'assets/icons/Pwa.svg',
-    'assets/icons/desktopMod.webp',
-    'assets/logo/anim1.svg',
-    'assets/logo/Logo2.webp',
-    'assets/logo/NFCdex_logo.webp',
-    'https://unpkg.com/aos@2.3.1/dist/aos.css'
+    '/index.html',
+    '/js/NFC.js',
+    '/js/formValidate.js',
+    '/css/font.css',
+    '/css/media-queries.css',
+    '/css/style.css',
+    '/css/variables.css',
+    '/assets/fonts/pokemon-ds/pokemon-ds-font.ttf',
+    '/assets/icons/PhoneScreen2.webp',
+    '/assets/icons/PhoneScreen2.png',
+    '/assets/icons/Pwa.svg',
+    '/assets/icons/desktopMod.webp',
+    '/assets/logo/anim1.svg',
+    '/assets/logo/Logo2.webp',
+    '/assets/logo/NFCdex_logo.webp',
 ]
 
 // Listener for the install event - pre-caches our assets list on service worker install.
@@ -26,7 +24,6 @@ self.addEventListener('install', event => {
     event.waitUntil((async () => {
         const cache = await caches.open(CACHE_NAME);
         await cache.addAll(PRECACHE_ASSETS);
-        self.skipWaiting();
     }));
 });
 
